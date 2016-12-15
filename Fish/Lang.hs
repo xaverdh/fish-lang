ae27a31a8c4ba0e89da941b360c64cbf7d7d9b73
Dominik Xaver Hörl <hoe.dom@gmx.de>
2016-12-15 10:40:19 +0100

    Allow varDef syntax (i.e. a[..]) in "set -e" command.

--- Fish/Lang.hs
+++ Fish/Lang.hs
@@ -84,7 +84,7 @@ data SetCommand t =
   --   boolean corresponds to the "-n" flag.
   | SetQuery (Maybe Scope) (Maybe Export) (Args t)
   -- ^ The /set/ builtin command in query mode
-  | SetErase (Maybe Scope) (Args t)
+  | SetErase (Maybe Scope) (N.NonEmpty (VarDef t))
   -- ^ The /set/ builtin command in erase mode
   deriving (Eq,Ord,Show,Functor,Generic)
 
